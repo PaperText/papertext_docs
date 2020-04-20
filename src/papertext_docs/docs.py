f9e809ef8c6ca39096b1c1aa88cec0bedc2397e5
Danil <danilunrandom@gmail.com>
2020-04-20 20:37:45 +0300

    fix: fixed module declaration to comply with standard

--- src/papertext_docs/docs.py
+++ src/papertext_docs/docs.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 from types import SimpleNamespace
-from typing import Callable, Optional, NoReturn, Mapping, Any
+from typing import Any, Callable, Mapping, NoReturn, Optional
 
 from fastapi import APIRouter
+
 from paperback import BaseDocs
 
 
 class DocsImplemented(BaseDocs):
-    requires_dir = True
+    requires_dir = False
+    DEFAULTS = {}
 
     def __init__(self, cfg: SimpleNamespace, storage_dir: Path):
         pass
