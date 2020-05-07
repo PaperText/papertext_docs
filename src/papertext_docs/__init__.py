ebcf13b837b27505e18b5a4e5cb7567c24490235
Danil Kireev <danilunrandom@gmail.com>
2020-05-07 21:35:24 +0300

    feat: changed pyexling dependency setup
    changed pyexling from git-submodule to poetry-dependency and removed unnecessary setup

--- src/papertext_docs/__init__.py
+++ src/papertext_docs/__init__.py
@@ -1,24 +1,8 @@
 """
     papertext module for managing corpuses/documents
-
-    isort:skip_file
 """
-
-from pathlib import Path
-from sys import path as sys_path
 
 from .docs import DocsImplemented
 
-src_path = Path(__file__) / ".." / ".."
-src_path = src_path.resolve()
-
-source_path = src_path / ".."
-source_path = source_path.resolve()
-
-pyexling_path = source_path / "pyexling" / "src"
-
-sys_path.append(str(pyexling_path))
-
-from pyexling import PyExLing  # noqa
 
 __version__ = "0.1.0"
