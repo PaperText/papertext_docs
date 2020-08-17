9101277cd777a9e684a760643a51db19af7e38c8
Danil Kireev <danilunrandom@gmail.com>
2020-08-17 13:09:17 +0300

    fix: moved to class-based scripts and isort ^5

--- src/papertext_docs/__main__.py
+++ src/papertext_docs/__main__.py
@@ -11,18 +11,38 @@ pyproject_path = source_path / "pyproject.toml"
 pyproject_path = pyproject_path.resolve()
 
 
-def lint():
-    call(f"python -m flakehell lint {src_path}".split(" "))
+class Scripts:
+    @staticmethod
+    def lint_flakehell():
+        print("+--------------------+")
+        print("| [flake]hell linter |")
+        print("+--------------------+")
+        call(f"python -m flakehell lint {src_path}".split(" "))
+
+    @staticmethod
+    def lint_mypy():
+        print("+-------------+")
+        print("| mypy linter |")
+        print("+-------------+")
+        call(f"python -m mypy {src_path}".split(" "))
+
+    @staticmethod
+    def lint():
+        Scripts.lint_flakehell()
+        Scripts.lint_mypy()
+
+    @staticmethod
+    def fix_black():
+        call(
+            f"python -m black {src_path} --config {pyproject_path}".split(" ")
+        )
+
+    @staticmethod
+    def fix_isort():
+        call(f"python -m isort {src_path}".split(" "))
 
 
-def fix_black():
-    call(f"python -m black {src_path} --config {pyproject_path}".split(" "))
-
-
-def fix_isort():
-    call(f"python -m isort -rc {src_path}".split(" "))
-
-
-def fix():
-    fix_black()
-    fix_isort()
+    @staticmethod
+    def fix():
+        Scripts.fix_black()
+        Scripts.fix_isort()
